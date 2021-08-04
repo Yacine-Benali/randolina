cf3837822aa82a1a8098278f11c691ce19624dc4
Yacine-Benali <benalidev@gmail.com>
2021-08-04 20:24:12 +0100

    user search for himself fixed

--- lib/app/home/feed/search_screen.dart
+++ lib/app/home/feed/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:randolina/app/home/feed/miniuser_to_profile.dart';
 import 'package:randolina/app/models/mini_user.dart';
+import 'package:randolina/app/models/user.dart';
 import 'package:randolina/services/algolia_service.dart';
 
 class DataSearch extends SearchDelegate<String> {
@@ -8,7 +10,6 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   List<Widget> buildActions(BuildContext context) {
-    // Actions for app bar
     return [
       IconButton(
           icon: Icon(Icons.clear),
@@ -29,66 +30,37 @@ class DataSearch extends SearchDelegate<String> {
     );
   }
 
-  //todo @average refactor this remove duplication
   @override
-  Widget buildResults(BuildContext context) {
+  Widget buildResults(BuildContext context) => buildResult(context);
+  @override
+  Widget buildSuggestions(BuildContext context) => buildResult(context);
+
+  Widget buildResult(BuildContext context) {
+    final User currentUser = context.read<User>();
     return FutureBuilder<List<MiniUser>>(
       future: algoliaService.performUserQuery(text: query),
       builder: (context, snapshot) {
         if (snapshot.hasData && snapshot.data != null) {
-          final movies = snapshot.data!.map((f) {
-            return ListTile(
-              leading: Image.network(f.profilePicture),
-              title: Text(f.name),
-              subtitle: Text(f.username),
-              onTap: () {
-                //! todo @high you cant search and click on yourself
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (builder) => MiniuserToProfile(miniUser: f),
-                  ),
-                );
-              },
-            );
+          final List<Widget> userTile = snapshot.data!.map((f) {
+            if (currentUser.id != f.id) {
+              return ListTile(
+                leading: Image.network(f.profilePicture),
+                title: Text(f.name),
+                subtitle: Text(f.username),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (builder) => MiniuserToProfile(miniUser: f),
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Container();
+            }
           }).toList();
 
-          return ListView(children: movies);
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text(snapshot.error.toString()),
-          );
-        }
-
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder<List<MiniUser>>(
-      future: algoliaService.performUserQuery(text: query),
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data != null) {
-          final movies = snapshot.data!.map((f) {
-            return ListTile(
-              leading: Image.network(f.profilePicture),
-              title: Text(f.name),
-              subtitle: Text(f.username),
-              onTap: () {
-                //! todo @high you cant search and click on yourself
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (builder) => MiniuserToProfile(miniUser: f),
-                  ),
-                );
-              },
-            );
-          }).toList();
-
-          return ListView(children: movies);
+          return ListView(children: userTile);
         } else if (snapshot.hasError) {
           return Center(
             child: Text(snapshot.error.toString()),
